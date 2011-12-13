17abb1fea95ed35bd17faba1cb1380be3842f238
Kyo Nagashima <kyo@hail2u.net>
2011-12-13 16:14:09 +0900

    make JSLint compatible

--- jquery.test-font.js
+++ jquery.test-font.js
@@ -1,26 +1,25 @@
-/*!
- * jQuery Plugin: Test Font - version 0.1
- * http://github.com/hail2u/jquery.test-font
- * Test font availability.
+/**
+ * @preserve jQuery Plugin: Test Font - version 0.2
  *
- * Copyright (c) 2011 Kyo Nagashima <kyo@hail2u.net>
- * This library licensed under MIT license:
- * http://opensource.org/licenses/mit-license.php
+ * LICENSE: http://hail2u.mit-license.org/2011
  */
 
+/*jslint indent: 2 */
+/*global jQuery, $ */
+
 (function ($) {
+  "use strict";
+
   $.testFont = function (fontname) {
-    var result = false;
-    var text   = "abcdefghijklmnopqrstuvwxyz";
-
-    var yardstick = $("<div/>").css({
-      "display":     "none",
-      "font-family": "jquery.test-font"
-    }).append(text).appendTo("body");
-
-    var test = yardstick.clone().css({
-      "font-family": fontname
-    }).appendTo("body");
+    var result = false,
+      text   = "abcdefghijklmnopqrstuvwxyz",
+      yardstick = $("<div/>").css({
+        "display":     "none",
+        "font-family": "jquery.test-font"
+      }).append(text).appendTo("body"),
+      test = yardstick.clone().css({
+        "font-family": fontname
+      }).appendTo("body");
 
     if (test.width() !== yardstick.width()) {
       result = true;
@@ -31,4 +30,4 @@
 
     return result;
   };
-})(jQuery);
+}(jQuery));
